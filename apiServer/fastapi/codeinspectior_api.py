1342e69d6e39ed46d0233abd17250adb09f83929
Kamal <kamal.tamang@berrybytes.com>
2026-05-04 17:35:00 +0545

    fix: cors issues

--- apiServer/fastapi/codeinspectior_api.py
+++ apiServer/fastapi/codeinspectior_api.py
@@ -183,14 +183,26 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://shininess-enroll-going.ngrok-free.dev",
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://localhost:8000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.middleware("http")
 
